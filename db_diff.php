1d1d57658b6b8d36709fa421c3ad40a5e11db445
Jonathan M. Hollin <darkblue@sdf.lonestar.org>
2013-11-10 19:41:35 +0000

    Changed indentation to spaces (Grrr!)

--- db_diff.php
+++ db_diff.php
@@ -10,32 +10,32 @@ spl_autoload_register();
 /**
  * Pre-flight
  */
-$db_1 = array	(
-								'host'			=> '127.0.0.1',
-								'username'	=> 'root',
-								'password'	=> 'root',
-								'database'	=> 'employees_1'
-							);
-$db_2 = array	(
-								'host'			=> '127.0.0.1',
-								'username'	=> 'root',
-								'password'	=> 'root',
-								'database'	=> 'employees_2'
-							);
+$db_1 = array (
+                'host'      => '127.0.0.1',
+                'username'  => 'root',
+                'password'  => 'root',
+                'database'  => 'employees_1'
+              );
+$db_2 = array (
+                'host'      => '127.0.0.1',
+                'username'  => 'root',
+                'password'  => 'root',
+                'database'  => 'employees_2'
+              );
 /*
-	If $config['strict'] is TRUE then the table schemas must match exactly
-	If $config['ignore_comments'] is TRUE then SQL COMMENTS will not be
-	considered diffs
-	If $config['ignore_auto_increment'] is TRUE then the AUTO_INCREMENT index
-	of the DB's tables will not be considered diffs
+  If $config['strict'] is TRUE then the table schemas must match exactly
+  If $config['ignore_comments'] is TRUE then SQL COMMENTS will not be
+  considered diffs
+  If $config['ignore_auto_increment'] is TRUE then the AUTO_INCREMENT index
+  of the DB's tables will not be considered diffs
 */
-$config = array	(
-									'left'									=> $db_1,
-									'right'									=> $db_2,
-									'strict'								=> FALSE,
-									'ignore_comments'				=> FALSE,
-									'ignore_auto_increment'	=> TRUE
-								);
+$config = array (
+                  'left'                  => $db_1,
+                  'right'                 => $db_2,
+                  'strict'                => FALSE,
+                  'ignore_comments'       => FALSE,
+                  'ignore_auto_increment' => TRUE
+                );
 $template_path = __DIR__ . '/templates/';
 
 
@@ -60,26 +60,26 @@ $data = array( 'diffs' => $MySQL_Diff->diffs );
  * Deal with the result (display or email it)
  */
 if ($MySQL_Diff->isCLI() === TRUE) {
-	// Running from the CLI
-	$what = 'differences were detected between the "' . $MySQL_Diff->diffs['left'] . '" and "' . $MySQL_Diff->diffs['right'] . '" databases';
-	$data['message'] = ($MySQL_Diff->mismatch === TRUE)
-		? '<p>The following ' . $what . ':</p><hr />'
-		: '<p>No ' . $what . '.</p>';
-	if ($MySQL_Diff->mismatch === TRUE) {
-		// Capture the result
-		ob_start();
-		View::factory($template_path . 'index', $data)->render();
-		$body = ob_get_clean();
-		// Email the result
-		mail($to, $subject, $body, $headers);
-		exit(1); // Let the CLI know that we detected DB differences
-	} else {
-		exit(0); // Let the CLI know that we terminated successfully
-	}
-	// Bash will have this return value in $?. Thus "$? -eq 0" can be used to
-	// determine whether or not Schema differences were detected
+  // Running from the CLI
+  $what = 'differences were detected between the "' . $MySQL_Diff->diffs['left'] . '" and "' . $MySQL_Diff->diffs['right'] . '" databases';
+  $data['message'] = ($MySQL_Diff->mismatch === TRUE)
+    ? '<p>The following ' . $what . ':</p><hr />'
+    : '<p>No ' . $what . '.</p>';
+  if ($MySQL_Diff->mismatch === TRUE) {
+    // Capture the result
+    ob_start();
+    View::factory($template_path . 'index', $data)->render();
+    $body = ob_get_clean();
+    // Email the result
+    mail($to, $subject, $body, $headers);
+    exit(1); // Let the CLI know that we detected DB differences
+  } else {
+    exit(0); // Let the CLI know that we terminated successfully
+  }
+  // Bash will have this return value in $?. Thus "$? -eq 0" can be used to
+  // determine whether or not Schema differences were detected
 } else {
-	// Running from the Web Browser
-	// Display the result
-	View::factory($template_path . 'index', $data)->render();
+  // Running from the Web Browser
+  // Display the result
+  View::factory($template_path . 'index', $data)->render();
 }
